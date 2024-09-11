7edc77165574726e1c1273151be35d785c45e7e9
Vitaliy V <vitaliyvilkov@gmail.com>
2024-09-11 14:27:49 +0400

    add tests

---
+++ src/test/java/ru/netology/order/OrderNegativeTest.java
@@ -0,0 +1,59 @@
+package ru.netology.order;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class OrderNegativeTest {
+
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    public void testEmptyName() {
+        $("[data-test-id=phone] input").setValue("+79006377457");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void testInvalidName() {
+        $("[data-test-id=name] input").setValue("Vitaliy Vilkov");
+        $("[data-test-id=phone] input").setValue("+79006377457");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    public void testEmptyPhone() {
+        $("[data-test-id=name] input").setValue("Виталий Вилков");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void testInvalidPhone() {
+        $("[data-test-id=name] input").setValue("Виталий Вилков");
+        $("[data-test-id=phone] input").setValue("+79006377457");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    public void testNoCheckbox() {
+        $("[data-test-id=name] input").setValue("Виталий-Вальдемар Вилков");
+        $("[data-test-id=phone] input").setValue("+79006377457");
+        $("button.button").click();
+        $("[data-test-id='agreement'].input_invalid").shouldBe(visible);
+    }
+}
---
+++ src/test/java/ru/netology/order/OrderPositiveTest.java
@@ -0,0 +1,20 @@
+package ru.netology.order;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class OrderPositiveTest {
+
+    @Test
+    public void testSuccessfulForm() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Виталий Вилков");
+        $("[data-test-id=phone] input").setValue("+79004567666");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
